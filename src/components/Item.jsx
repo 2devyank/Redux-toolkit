84842c668de06c02ca4aab7680b7ffe9c73e9396
devyank <devyanknagpal2002@gmail.com>
2022-11-12 00:51:30 +0530

    styling

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,16 +5,28 @@ import { addtocart } from '../features/bag/bag';
 const Item = ({title,img,price,id}) => {
   const dispatch=useDispatch();
   return (
-    <div className="item">
-      <div className='rox'>
-        <img src={img} style={{height:"50px",width:"50px"}} alt="" />
-        <p>{title}</p>
+    <table className="item">
+      {/* <div className='rox'> */}
+        <tr>
+         <td>
+           <img src={img} style={{height:"50px",width:"50px"}} alt="" />
+          </td>
+          
+       <td>
 
-      </div>
+          <p>{title}</p>
+       </td>
+      {/* </div> */}
 
-        <p>{price}</p>
+<td>
+          <p className='price'>{price}</p>
+  </td>
+<td>
+
         <button className='buts' onClick={()=>dispatch(addtocart({id}))}>Add To Cart</button>
-        </div>
+</td>
+          </tr>
+        </table>
   )
 }
 
